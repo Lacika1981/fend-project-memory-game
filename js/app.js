656ed78ec9f5cc82119fa265a03688e898e4a7a3
Laszlo Varga <vargalaszlo1981@googlemail.com>
2017-12-24 07:55:09 +0000

    all basic functionality is done

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@
  * Create a list that holds all of your cards
  */
 
-
- const parentOfCards = document.getElementById('deck');
- const cards = document.getElementsByClassName('card');
- let cardsArray = Array.from(cards);
- console.log(cardsArray);
-
+const parentOfCards = document.getElementById('deck');
+const cards = document.getElementsByClassName('card');
+const counter = document.getElementsByClassName('moves');
+const result = document.getElementsByClassName('result');
+let cardsArray = Array.from(cards);
+console.log(cardsArray);
 
 /*
  * Display the cards on the page
@@ -16,12 +16,14 @@
  *   - add each card's HTML to the page
  */
 
- cardsArray = shuffle(cardsArray);
- cardsArray.map(e => parentOfCards.appendChild(e));
+cardsArray = shuffle(cardsArray);
+cardsArray.map(e => parentOfCards.appendChild(e));
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -34,7 +36,6 @@ function shuffle(array) {
     return array;
 }
 
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -46,27 +47,56 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
- listOfOpenCards = [];
+listOfOpenCards = [];
+numberOfOpenCards = 16;
+counter[0].textContent = 0;
 
- function checkMatch(){
-     console.log(listOfOpenCards[0].firstElementChild.classList[1], listOfOpenCards[1].firstElementChild.classList[1]);
-     if (listOfOpenCards[0].firstElementChild.classList[1] === listOfOpenCards[1].firstElementChild.classList[1]){
-        listOfOpenCards[0].className += ' match';
-        listOfOpenCards[1].className += ' match';
+const cardLeftToOpen = () => {
+    if (numberOfOpenCards === 0){
+        result[0].classList.remove('hidden');
+    }
+}
+
+const updateMovesCounter = () => {
+    if (numberOfOpenCards !== 0) {
+    counter[0].textContent++;
+    }
+}
+
+const cardsNotMatched = () => {
+    setTimeout(() => {
+        listOfOpenCards.map((e) => e.classList.remove('show', 'open')); // it prevents the event when user clciks too fast and put more than two card into the 'listOfOpenCards' Array
         listOfOpenCards.length = 0;
-     }
- }
+    }, 250);
 
+}
 
- function showCard(e){
-     console.log(e);
+const lockMatchedCards = (card1, card2) => {
+    numberOfOpenCards -= 2;
+    cardLeftToOpen();
+    listOfOpenCards.map((e) => e.classList.add('match'));
+    listOfOpenCards.length = 0;
+}
+
+const checkMatch = () => {
+    if (listOfOpenCards[0].firstElementChild.classList[1] === listOfOpenCards[1].firstElementChild.classList[1]) {
+        lockMatchedCards(listOfOpenCards[0], listOfOpenCards[1])
+    } else {
+        cardsNotMatched();
+    }
+}
+
+const showCard = (e) => {
+    if (e.target.className !== 'card open show'){ //prevent to click the same element twice - simple but works
+    updateMovesCounter();
     listOfOpenCards.push(e.target);
-    e.target.className += ' open show';
+    e.target.classList.add('open', 'show');
     console.log(listOfOpenCards.length);
-    if (listOfOpenCards.length === 2){
+    if (listOfOpenCards.length === 2) {
         checkMatch();
     }
- }
+}
+}
 
 for (i = 0; i < cards.length; i++) {
     cards[i].addEventListener("click", showCard);
