3699194709638244d7c16d3c79bb8db65cd7c9ff
Laszlo Varga <vargalaszlo1981@googlemail.com>
2017-12-25 12:57:19 +0000

    adding all functionality but timer

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ const parentOfCards = document.getElementById('deck');
 const cards = document.getElementsByClassName('card');
 const counter = document.getElementsByClassName('moves');
 const result = document.getElementsByClassName('result');
+const reset = document.getElementsByClassName('restart');
+const stars = document.getElementsByClassName('stars');
 let cardsArray = Array.from(cards);
 console.log(cardsArray);
 
@@ -50,22 +52,58 @@ function shuffle(array) {
 listOfOpenCards = [];
 numberOfOpenCards = 16;
 counter[0].textContent = 0;
+let prevE = 0;
+
+const showStars = (e) => {
+    console.log(prevE);
+    let nextE = e;
+    if (prevE !== nextE) {
+        prevE = nextE;
+        stars[0].innerHTML = ''; //https://stackoverflow.com/questions/18795028/javascript-remove-li-without-removing-ul
+        console.log(`prevE: ${prevE}, nextE: ${nextE}`);
+        for (let i = 1; i <= nextE; i++) {
+            const starLi = document.createElement('li');
+            const starI = document.createElement('i');
+            starI.setAttribute('class', 'fa fa-star');
+            starLi.appendChild(starI);
+            stars[0].appendChild(starLi);
+        }
+    }
+}
+
+showStars(3);
+
+const resetGame = () => {
+    numberOfOpenCards = 16;
+    counter[0].textContent = 0;
+    cardsArray.forEach(e => e.classList.remove('show', 'open', 'match'));
+    listOfOpenCards.length = 0;
+    cardsArray = shuffle(cardsArray);
+    cardsArray.map(e => parentOfCards.appendChild(e));
+    result[0].classList.add('hidden');
+    showStars(3);
+}
 
 const cardLeftToOpen = () => {
-    if (numberOfOpenCards === 0){
-        result[0].classList.remove('hidden');
+    if (numberOfOpenCards === 0) {
+        result[0]
+            .classList
+            .remove('hidden');
     }
 }
 
 const updateMovesCounter = () => {
+    counter[0].textContent >= 36
+        ? showStars(1)
+        : counter[0].textContent >= 28 ? showStars(2) : showStars(3);
     if (numberOfOpenCards !== 0) {
-    counter[0].textContent++;
+        counter[0].textContent++;
     }
 }
 
 const cardsNotMatched = () => {
     setTimeout(() => {
-        listOfOpenCards.map((e) => e.classList.remove('show', 'open')); // it prevents the event when user clciks too fast and put more than two card into the 'listOfOpenCards' Array
+        listOfOpenCards.forEach((e) => e.classList.remove('show', 'open')); // it prevents the event when user clciks too fast and put more than two card into the 'listOfOpenCards' Array
         listOfOpenCards.length = 0;
     }, 250);
 
@@ -74,7 +112,7 @@ const cardsNotMatched = () => {
 const lockMatchedCards = (card1, card2) => {
     numberOfOpenCards -= 2;
     cardLeftToOpen();
-    listOfOpenCards.map((e) => e.classList.add('match'));
+    listOfOpenCards.forEach((e) => e.classList.add('match'));
     listOfOpenCards.length = 0;
 }
 
@@ -87,17 +125,19 @@ const checkMatch = () => {
 }
 
 const showCard = (e) => {
-    if (e.target.className !== 'card open show'){ //prevent to click the same element twice - simple but works
-    updateMovesCounter();
-    listOfOpenCards.push(e.target);
-    e.target.classList.add('open', 'show');
-    console.log(listOfOpenCards.length);
-    if (listOfOpenCards.length === 2) {
-        checkMatch();
+    if (e.target.className !== 'card open show') { //prevent to click the same element twice - simple but works
+        updateMovesCounter();
+        listOfOpenCards.push(e.target);
+        e.target.classList.add('open', 'show');
+        console.log(listOfOpenCards.length);
+        if (listOfOpenCards.length === 2) {
+            checkMatch();
+        }
     }
 }
+
+for (i = 0; i < cardsArray.length; i++) {
+    cardsArray[i].addEventListener("click", showCard);
 }
 
-for (i = 0; i < cards.length; i++) {
-    cards[i].addEventListener("click", showCard);
-}
+reset[0].addEventListener('click', resetGame);
