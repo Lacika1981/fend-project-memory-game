dd5277bda3a1959b06d22d6270fa0687e7a1f82d
Laszlo Varga <vargalaszlo1981@googlemail.com>
2017-12-26 21:47:02 +0000

    stopwatch is not working, anything else is done

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@
  * Create a list that holds all of your cards
  */
 
-const parentOfCards = document.getElementById('deck');
+const parentOfCards = document.getElementsByClassName('deck')[0];
 const cards = document.getElementsByClassName('card');
-const counter = document.getElementsByClassName('moves');
-const result = document.getElementsByClassName('result');
-const reset = document.getElementsByClassName('restart');
+const moves = document.getElementsByClassName('moves');
+const result = document.getElementsByClassName('result')[0];
+const restart = document.getElementsByClassName('restart');
 const stars = document.getElementsByClassName('stars');
 let cardsArray = Array.from(cards);
 console.log(cardsArray);
@@ -51,7 +51,6 @@ function shuffle(array) {
 
 let listOfOpenCards = [];
 let numberOfOpenCards = 16;
-counter[0].textContent = 0;
 let prevE = 0;
 
 const showStars = (e) => {
@@ -59,8 +58,9 @@ const showStars = (e) => {
     let nextE = e;
     if (prevE !== nextE) {
         prevE = nextE;
-        stars[0].innerHTML = ''; //https://stackoverflow.com/questions/18795028/javascript-remove-li-without-removing-ul
-        console.log(`prevE: ${prevE}, nextE: ${nextE}`);
+        for (let i = 0; i < stars.length; i++) {
+            stars[i].innerHTML = ''; //https://stackoverflow.com/questions/18795028/javascript-remove-li-without-removing-ul
+        }
         for (let i = 1; i <= nextE; i++) {
             const starLi = document.createElement('li');
             const starI = document.createElement('i');
@@ -68,34 +68,57 @@ const showStars = (e) => {
             starLi.appendChild(starI);
             stars[0].appendChild(starLi);
         }
+        for (let j = 1; j <= nextE; j++) {
+            const starLi = document.createElement('li');
+            const starI = document.createElement('i');
+            starI.setAttribute('class', 'fa fa-star');
+            starLi.appendChild(starI);
+            stars[1].appendChild(starLi);
+        }
     }
 };
 
-showStars(3);
+showStars(3); //start value for the stars
 
 const resetGame = () => {
     numberOfOpenCards = 16;
-    counter[0].textContent = 0;
+    for (let i = 0; i < moves.length; i++) {
+        moves[i].textContent = 0;
+    }
     cardsArray.forEach(e => e.classList.remove('show', 'open', 'match'));
     listOfOpenCards.length = 0;
     cardsArray = shuffle(cardsArray);
     cardsArray.map(e => parentOfCards.appendChild(e));
-    result[0].classList.add('hidden');
-    my_stopwatch.reset();
+    result
+        .classList
+        .add('hidden');
     showStars(3);
+    $('#exampleModal').modal('hide');
+    my_stopwatch.reset();
+    my_stopwatch2.reset();
 };
 
 const cardLeftToOpen = () => {
-    if (numberOfOpenCards === 0) {
-        result[0].classList.remove('hidden');
+    if (numberOfOpenCards === 14) {
+        result
+            .classList
+            .remove('hidden');
         my_stopwatch.stop();
+        my_stopwatch2.stop();
+        $('#exampleModal').modal('show');
     }
 };
 
 const updateMovesCounter = () => {
-    counter[0].textContent >= 36 ? showStars(1) : counter[0].textContent >= 28 ? showStars(2) : showStars(3);
+    moves.textContent >= 36
+        ? showStars(1)
+        : moves.textContent >= 28
+            ? showStars(2)
+            : showStars(3);
     if (numberOfOpenCards !== 0) {
-        counter[0].textContent++;
+        for (let i = 0; i < moves.length; i++) {
+            moves[i].textContent++;
+        }
     }
 };
 
@@ -123,13 +146,17 @@ const checkMatch = () => {
 };
 
 const showCard = (e) => {
-    if (counter[0].textContent == 0) { //itdoes not want to wor with strict equal - no idea why - scratching my head
+    if (moves.textContent == 0) { //it does not want to work with strict equal - no idea why - scratching my head
         my_stopwatch.start();
+        my_stopwatch2.start();
     }
     if (e.target.className !== 'card open show') { //prevent to click the same element twice - simple but works
         updateMovesCounter();
         listOfOpenCards.push(e.target);
-        e.target.classList.add('open', 'show');
+        e
+            .target
+            .classList
+            .add('open', 'show');
         console.log(listOfOpenCards.length);
         if (listOfOpenCards.length === 2) {
             checkMatch();
@@ -141,10 +168,12 @@ for (let i = 0; i < cardsArray.length; i++) {
     cardsArray[i].addEventListener("click", showCard);
 }
 
-reset[0].addEventListener('click', resetGame);
+for (let i = 0; i < restart.length; i++) {
+    restart[i].addEventListener('click', resetGame);
+}
 
 //https://gist.github.com/electricg/4372563
-const stopwatch = function (my_element_id) {
+const stopwatch = (my_element_id) => {
     const $time = document.getElementById(my_element_id);
     if (!$time) 
         return;
@@ -176,17 +205,17 @@ const stopwatch = function (my_element_id) {
         $time.innerHTML = formatTime();
     }
 
-    api.reset = function () {
+    api.reset = () => {
         api.stop();
         $time.innerHTML = '00:00:00:00';
         time = 0;
     };
 
-    api.start = function () {
+    api.start = () => {
         clocktimer = setInterval(update, duration);
     };
 
-    api.stop = function () {
+    api.stop = () => {
         clearInterval(clocktimer);
     };
 
@@ -196,3 +225,4 @@ const stopwatch = function (my_element_id) {
 };
 
 const my_stopwatch = stopwatch('time');
+const my_stopwatch2 = stopwatch('time2');
